778041b6c28f488ef2e239de1140a5df9d962275
Mathivanan <mathivanansakthivel10@gmail.com>
2023-06-10 06:03:03 +0530

    parameter approach in dataDrivenFramework

---
+++ src/loginTestCases/BothCorrect.java
@@ -0,0 +1,29 @@
+package loginTestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class BothCorrect {
+
+	@Test
+	@Parameters({ "username", "password" })
+	public void LoginWithBothCorrect(String uName, String pWord) {
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium_training\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://practicetestautomation.com/practice-test-login/");
+		WebElement userName = driver.findElement(By.id("username"));
+		userName.sendKeys(uName);
+
+		WebElement password = driver.findElement(By.id("password"));
+		password.sendKeys(pWord);
+
+		WebElement button = driver.findElement(By.id("submit"));
+		button.click();
+
+	}
+}
---
+++ src/loginTestCases/CorrectPasswordTestCase.java
@@ -0,0 +1,28 @@
+package loginTestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class CorrectPasswordTestCase {
+	@Test
+	@Parameters({ "username", "password" })
+	public void LoginWithCorrectPassword(String uName, String pWord) {
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium_training\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://practicetestautomation.com/practice-test-login/");
+		WebElement userName = driver.findElement(By.id("username"));
+		userName.sendKeys(uName);
+
+		WebElement password = driver.findElement(By.id("password"));
+		password.sendKeys(pWord);
+
+		WebElement button = driver.findElement(By.id("submit"));
+		button.click();
+
+	}
+}
